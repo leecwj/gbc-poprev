f5a69a1c359680cdf8df5440c005650c614ddf0f
leecwj <37468566+leecwj@users.noreply.github.com>
2020-11-25 12:02:15 +1000

    docstrings and type hints

--- colour_selector.py
+++ colour_selector.py
@@ -1,11 +1,27 @@
 import tkinter as tk
+from typing import Callable, List, Union
 
 from constants import CSO_WIDTH, CSO_HEIGHT
 
 
 class ColourSelector(tk.Frame):
+    """
+    A GUI component which allows the user to select one of several colours.
+    """
 
-    def __init__(self, master, colours, highlights, callback, bg="#ffffff"):
+    def __init__(self, master, colours: List[str], highlights: List[str],
+                 callback: Callable[[int], None], bg: str = "#ffffff"):
+        """
+        Initialise this ColourSelector
+        :param master: parent container of this ColourSelector
+        :param colours: a list of hex strings representing the colours
+        available to choose from
+        :param highlights: a list of hex strings such that highlights[i] is
+        the highlight colour corresponding to colours[i]
+        :param callback: function which will be called to notify that a certain
+        colour has been chosen, and which colour it is
+        :param bg: background colour of this ColourSelector
+        """
         super().__init__(master, bg=bg, pady=5)
 
         self._options2 = []
@@ -18,14 +34,26 @@ class ColourSelector(tk.Frame):
             cso.pack(side=tk.LEFT)
             self._options2.append(cso)
 
-    def handle_click_callback(self, identifier):
+    def handle_click_callback(self, identifier: int) -> None:
+        """
+        Handles the event in which the user clicks on one of the options in
+        this ColourSelector
+        :param identifier: the ID of the colour the user clicked on
+        """
         self.set_selected(identifier)
         self._callback(identifier)
 
-    def get_selected(self):
+    def get_selected(self) -> Union[int, None]:
+        """
+        :return: the ID of the currently selected colour
+        """
         return self._selected
 
-    def set_selected(self, identifier):
+    def set_selected(self, identifier: Union[int, None]) -> None:
+        """
+        Change the selected colour of this ColourSelector
+        :param identifier: the ID of the colour to set as selected
+        """
         if self._selected is not None:
             self._options2[self._selected].set_selected(False)
 
@@ -34,13 +62,30 @@ class ColourSelector(tk.Frame):
 
         self._selected = identifier
 
-    def clear_selected(self):
+    def clear_selected(self) -> None:
+        """
+        Clear the selected colour of this ColourSelector
+        """
         self.set_selected(None)
 
 
 class ColourSelectorOption(tk.Frame):
+    """
+    A component of the ColourSelector class, which represents a colour that
+    the user may select.
+    """
 
-    def __init__(self, master, colour, highlight, callback, identifier):
+    def __init__(self, master, colour: str, highlight: str,
+                 callback: Callable[[int], None], identifier: int):
+        """
+        Initialise this ColourSelectorOption
+        :param master: parent container of this ColourSelectorOption
+        :param colour: the colour this option represents
+        :param highlight: the colour to highlight this option when it is
+        selected
+        :param callback: the function to call when this option is clicked
+        :param identifier: the ID of this option
+        """
         super().__init__(master, bg=colour, height=CSO_HEIGHT, width=CSO_WIDTH)
 
         self._callback = callback
@@ -58,17 +103,32 @@ class ColourSelectorOption(tk.Frame):
         self.bind("<Button-1>", self.handle_click)
         self._label.bind("<Button-1>", self.handle_click)
 
-    def handle_click(self, evt):
+    def handle_click(self, evt: tk.Event) -> None:
+        """
+        Handle the event in which this ColourSelectorOption is clicked
+        :param evt: event object generated when this object is clicked
+        """
         self._callback(self._identifier)
 
-    def toggle_selected(self):
+    def toggle_selected(self) -> None:
+        """
+        Toggle this option as selected / not selected
+        """
         self._selected = not self._selected
 
-    def set_selected(self, value):
+    def set_selected(self, value: bool) -> None:
+        """
+        Set whether or not this option is selected
+        :param value: boolean indicating whether this option is selected or not
+        """
         self._selected = value
         self.update_look()
 
-    def update_look(self):
+    def update_look(self) -> None:
+        """
+        Update the appearance of this ColourSelectorOption to indicate whether
+        or not it is selected
+        """
         if self._selected:
             self._label.config(text="╳️")
         else:
