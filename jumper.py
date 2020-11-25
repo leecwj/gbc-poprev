f5c7bf1398ab37fa0a869b988f80ea3e2621fd28
leecwj <37468566+leecwj@users.noreply.github.com>
2020-11-25 15:36:57 +1000

    docstrings and type hints

--- jumper.py
+++ jumper.py
@@ -1,12 +1,29 @@
 import tkinter as tk
+from typing import Callable
 
 from constants import DRAW_WIDTH, DRAW_HEIGHT
 
 
 class Jumper(tk.Frame):
+    """
+    A GUI component that allows the user to specify and "jump" to an
+    (x, y) coordinate.
+    """
 
-    def __init__(self, master, jump_callback,
-                 xl=1, xu=DRAW_WIDTH, yl=1, yu=DRAW_HEIGHT, bg="#ffffff"):
+    def __init__(self, master, jump_callback: Callable[[int, int], None],
+                 xl: int = 1, xu: int = DRAW_WIDTH, yl: int = 1,
+                 yu: int = DRAW_HEIGHT, bg: str = "#ffffff"):
+        """
+        Initialise this Jumper
+        :param master: parent container of this Jumper
+        :param jump_callback: function to call when the user clicks the jump
+        button
+        :param xl: lower bound on the allowed range of x coordinates
+        :param xu: upper bound on the allowed range of x coordinates
+        :param yl: lower bound on the allowed range of y coordinates
+        :param yu: upper bound on the allowed range of y coordinates
+        :param bg: background colour of this Jumper
+        """
         super().__init__(master, bg=bg)
 
         self._xl = xl
@@ -31,7 +48,10 @@ class Jumper(tk.Frame):
         jump_btn = tk.Button(self, text="Jump", command=self.click_callback)
         jump_btn.pack(side=tk.LEFT)
 
-    def click_callback(self):
+    def click_callback(self) -> None:
+        """
+        Handles the event when the jump button is clicked
+        """
         x = self._xentry.get()
         y = self._yentry.get()
 
@@ -46,7 +66,12 @@ class Jumper(tk.Frame):
 
         self._jump_callback(x, y)
 
-    def display_position(self, x, y):
+    def display_position(self, x: int, y: int) -> None:
+        """
+        Display a given (x, y) coordinate in this object's text entry fields.
+        :param x: x coordinate to display
+        :param y: y coordinate to display
+        """
         self._xentry.delete(0, tk.END)
         self._xentry.insert(0, str(x))
 
